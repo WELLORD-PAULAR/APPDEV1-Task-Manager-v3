1d53cc7419215921c88959fa9df3313aef39054a
WELLORD-PAULAR <20187455@s.ubaguio.edu>
2025-10-14 12:33:49 +0800

    second commit, changes to task service implementation, satisfying numbers 1 & 2.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Task } from './task/task';
+import { TaskFilter } from './task-filter/task-filter';
+import { TaskList } from './task-list/task-list';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home},
+    { path: 'tasks', component: TaskList},
+    { path: 'task', component: Task},
+    { path: 'filter', component: TaskFilter}
+];
---
+++ src/app/task-service/task-service.ts
@@ -0,0 +1,39 @@
+import { Injectable, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private tasks = signal<Array<{ id: number ; description?: string; createdAt?: string; isCompleted: boolean }>>([]);
+  private nextId = 1;
+
+  getUniqueId() {
+    return this.nextId++;
+  }
+
+  getTasks() {
+    return this.tasks;
+  }
+
+  addTask(task: { description?: string; createdAt?: string }) {
+    const newTask = {
+      id: this.getUniqueId(),
+      description: task.description,
+      createdAt: Date.now().toString(),
+      isCompleted: false
+    };
+    this.tasks.update(tasks => [...tasks, newTask]);
+  }
+
+  editTask(updatedTask: { id: number; description?: string }) {
+    this.tasks.update(tasks => tasks.map(task => task.id === updatedTask.id ? { ...task, description: updatedTask.description } : task));
+  }
+
+  currentFilter = signal<'all' | 'completed' | 'pending'>('all');
+
+  setFilter(filter: 'all' | 'completed' | 'pending') {
+    this.currentFilter.set(filter);
+  }
+
+  
+}
